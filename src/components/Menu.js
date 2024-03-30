9f8f1f19c98ca62437087f23bbedfd628653a7a1
EdCamarillo <edwardjohn.camarillo@cit.edu>
2024-03-30 21:13:12 +0800

    styled menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import { Button } from '@mui/material';
+import { buttonStyles } from './styled_components/Styles.js';
 
 function Menu() {
   return (
-    <div>
+    <div className="menu-container">
       <h1>Menu</h1>
-      <Link to="/login">
-        <button>Login</button>
-      </Link>
-      <Link to="/register">
-        <button>Register</button>
-      </Link>
+      <Button
+        variant="contained"
+        component={Link}
+        to={`/login`}
+        sx={{
+            ...buttonStyles,
+            width: '200px',
+        }}
+      >
+        Login
+      </Button>
+      <Button
+        variant="contained"
+        component={Link}
+        to={`/register`}
+        sx={{
+            ...buttonStyles,
+            width: '200px',
+        }}
+      >
+        Register
+      </Button>
     </div>
   );
 }
